1949bebdccf43ec85a2fcf24497cc16dba742b63
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-02-20 08:07:19 +0000

    投稿編集機能実装完了→星評価の実装開始から

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -31,12 +31,26 @@ class Users::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "登録内容を更新しました！"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.user_id = current_user.id
+       @post.destroy
+       flash[:notice] = "投稿を削除しました"
+       redirect_to posts_path
+    end
   end
 
   private
