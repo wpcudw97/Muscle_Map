1bdef137b27deb45c0f3e5ec016fc71c082e26fe
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-02-20 11:18:58 +0000

    いいね機能実装開始

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,9 @@ import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
 
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
