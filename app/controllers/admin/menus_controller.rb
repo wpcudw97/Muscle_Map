bd00707ba6e58e77ac369c24b5599bfa44cf0fc1
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-02-22 14:56:20 +0000

    メニュー一覧実装完了→メニュー作成機能実装から

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -1,22 +1,41 @@
 class Admin::MenusController < ApplicationController
   def index
+    @menus = Menu.all
   end
 
   def new
+    @menu = Menu.new
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    @menu.save
+    redirect_to admin_menus_path
   end
 
   def show
+    @menu = Menu.find(params[:id])
   end
 
   def edit
+    @menu = Menu.find(params[:id])
   end
 
   def update
+    @menu = Menu.find(params[:id])
+    @menu.update(menu_params)
+    redirect_to admin_menus_path
   end
 
   def destroy
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    flash[:notice] = "メニューを削除しました"
+    redirect_to admin_menus_path
   end
+
+  def menu_params
+    params.require(:menu).permit(:genre_id, :title, :body, :menu_image)
+  end
+
 end
--- app/helpers/user/training_helper.rb
+++
@@ -1,2 +0,0 @@
-module User::TrainingHelper
-end
