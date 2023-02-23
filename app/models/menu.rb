5366e328766271f2c1592d2a592352b916f2fc03
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-02-23 06:44:30 +0000

    メニュー一覧　メニュー作成　メニュー編集 メニュー詳細機能実装

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,11 +1,11 @@
 class Menu < ApplicationRecord
   belongs_to :genre
-  has_one_attached :item_image
+  has_one_attached :menu_image
 
   def get_menu_image(width,height)
     unless menu_image.attached?
-      file_path = Rails.root.join('app/assets/images/noimage.jpeg')
-      menu_image.attach(io: File.open(file_path), filename: 'noimage.jpeg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      menu_image.attach(io: File.open(file_path), filename: 'no_image.jpeg', content_type: 'image/jpeg')
     end
     menu_image.variant(resize_to_limit: [width,height]).processed
   end
