3fa3686020416837f1a55d27f6f719609a39c6f7
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-02-27 03:58:42 +0000

    検索機能実装完了→footerが上に来るのはなぜ？

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,4 @@
-class SearchesController <
+class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
--- app/models/post.rb
+++ app/models/post.rb
@@ -16,6 +16,19 @@ class Post < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
-
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("title LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post = Post.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post = Post.where("title LIKE?","%#{word}%")
+    else
+      @post = Post.all
+    end
+  end
 
 end
