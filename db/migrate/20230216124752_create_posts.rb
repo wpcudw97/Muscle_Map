eb2543d4a39ef62e853400875bbc51cd97f62bc4
wpcudw97 <114921824+wpcudw97@users.noreply.github.com>
2023-03-13 15:23:14 +0000

    migrate見直し

--- db/migrate/20230216124752_create_posts.rb
+++ db/migrate/20230216124752_create_posts.rb
@@ -2,11 +2,9 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.integer :user_id, null: false
-      t.integer :menu_id, null: false
       t.string :title, null: false
       t.string :body, null: false
       t.string :post_image
-      t.float :rate, null: false, default: 0
       t.timestamps
     end
   end
